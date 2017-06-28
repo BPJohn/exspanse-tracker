234c5bfe5421871bb853217bd1c34416f10c6dbf
BPJohn <bryanpj@att.net>
2017-06-28 14:12:11 -0500

    working on angular data input for interface

--- public/javascripts/controllers/fuel-controller.js
+++ public/javascripts/controllers/fuel-controller.js
@@ -2,4 +2,8 @@ var fuelController = function($scope){
   console.log("Inside fuel controller");
   $scope.message = "Milage Calulator ";
   $scope.prevOdomiter = 25365 ;
-}
+  // $scope.milesdrove = $scope.prevOdomiter - $scope.currentOdomiter;
+  // console.log('Miles Drive '+ milesdrove);
+
+
+};
