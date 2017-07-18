98e312964b3b35c6f7a14b089fee5e3a253244b7
BPJohn <bryanpj@att.net>
2017-07-17 19:05:08 -0500

    reverted changes

--- public/javascripts/controllers/fuel-controller.js
+++ public/javascripts/controllers/fuel-controller.js
@@ -1,26 +1,26 @@
 app.controller('fuelController', ['$scope', '$resource', function($scope, $resource) {
-  var FuelLog = $resource('/api/fuellog');
+  var FuelLog = $resource('/api/FuelLog');
   $scope.message = "Welcome to the Fuel Log Page";
   $scope.startingOdomiter = 56324;
   // console.log("Inside fuel controller");
 
   FuelLog.query(function(results) {
-    $scope.fulelog = results;
+    $scope.fulelogs = results;
   })
-  $scope.fuellog = []
+  $scope.fuellogs = []
 
 
   $scope.logFuel = function() {
 
 
-    var fuellog = new FuelLog();
-    fuellog.created_date = $scope.logDate;
-    fuellog.odomiter = $scope.currentOdomiter;
-    fuellog.gallons = $scope.qtyGallons;
-    fuellog.cost = $scope.fuelCost;
+    var fuellogs = new FuelLog();
+    fuellogs.created_date = $scope.logDate;
+    fuellogs.odomiter = $scope.currentOdomiter;
+    fuellogs.gallons = $scope.qtyGallons;
+    fuellogs.cost = $scope.fuelCost;
 
-    fuellog.$save(function(result) {
-      $scope.fuellog.push(result);
+    fuellogs.$save(function(result) {
+      $scope.fuellogs.push(result);
       //  {odomiter:$scope.currentOdomiter,
       //  gallons:$scope.qtyGallons,
       //  cost:$scope.fuelCost}
