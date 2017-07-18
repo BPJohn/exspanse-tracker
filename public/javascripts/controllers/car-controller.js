237bee2216d2a6ddb9c0c633c01eaa4dce8df62f
BPJohn <bryanpj@att.net>
2017-07-18 10:35:19 -0500

    fixed more issues

--- public/javascripts/controllers/car-controller.js
+++ public/javascripts/controllers/car-controller.js
@@ -2,14 +2,14 @@ var CarLog = require('../models/car-model');
 
 module.exports.create = function(req , res){
   var carlog = new CarLog(req.body);
-  carlog.save(function(err , results){
-    res.json(results);
+  carlog.save(function(err , result){
+    res.json(result);
 
   });
   console.log(req.body);
 }
 module.exports.list = function(req , res){
-  carlog.find({},function(err , results){
+  CarLog.find({},function(err , results){
     res.json(results);
   })
 
--- public/javascripts/controllers/carController.js
+++ public/javascripts/controllers/carController.js
@@ -5,12 +5,15 @@ app.controller('carController',['$scope','$resource',function($scope ,$resource)
   // CarLog.query(function(results){
   //   $scope.carlog = results;
   // })
-  $scope.Carlog =[]
+  $scope.carlog =[]
+
+  $scope.createCar = function(){
 
   var carlog = new CarLog();
   carlog.make = $scope.carMake;
   carlog.model = $scope.carModel;
   carlog.year = $scope.carYear;
   carlog.startMilage = $scope.openMilage
-  mongoose.connect('mongodb://localhost:27017/logFuel/' + $scope.carModel);
+  // mongoose.connect('mongodb://localhost:27017/logFuel/' + $scope.carModel);
+}
 }])
